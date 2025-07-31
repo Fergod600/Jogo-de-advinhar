f59a40231644c76f5d1798d9aa8677c60fcceac0
Fernando <fernandodev600@gmail.com>
2025-07-31 10:06:41 -0300

    Criando Componente de Letter

--- classroom/src/App.tsx
+++ classroom/src/App.tsx
@@ -1,6 +1,7 @@
 import styles from "./app.module.css"
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 export function App() {
 
@@ -14,6 +15,12 @@ export function App() {
                 <Header current={2} max={10} onRestart={handleRestartGame} />
                 
                 <Tip tip="Uma das linguagens de programação mais utilizadas" />
+            
+                <Letter value = "R"/>
+                <Letter value = "E"/>
+                <Letter value = "A"/>
+                <Letter value = "C"/>
+                <Letter value = "T"/>
             </main>
         </div>
     )
